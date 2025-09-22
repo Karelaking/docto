08e117f317712734e0ac3aa1e8d865894cb16a87
mradul kumar katiyar <dev.mradul@proton.me>
2025-09-22 21:03:41 +0530

    login and signin complete

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import NavBar from "@/components/nav-bar";
+import React from "react";
+
+const layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="min-w-full min-h-screen flex items-center justify-center flex-col overflow-auto no-scrollbar">
+      <NavBar/>
+      <div className="min-w-6xl">{children}</div>
+    </div>
+  );
+};
+
+export default layout;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none" /* Internet Explorer 10+ */,
+          "scrollbar-width": "none" /* Firefox */,
+          "&::-webkit-scrollbar": {
+            display: "none" /* Chrome, Safari, Edge */,
+          },
+        },
+      });
+    },
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+    },
+  },
+};
