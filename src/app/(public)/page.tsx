c9823cc9dd8fe56cb4887ebf9da1028ed17d7d51
mradul kumar katiyar <dev.mradul@proton.me>
2025-09-23 08:17:57 +0530

    fix some carousel issues

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,7 @@ import AutoPlayCarousel from "@/components/autoplay-carousel";
 
 export default function Home() {
   return (
-    <section className="w-full min-h-screen flex flex-col">
+    <section className="w-full min-h-screen h-full flex flex-col">
       <AutoPlayCarousel/>
     </section>
   );
--- src/components/autoplay-carousel.tsx
+++ src/components/autoplay-carousel.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
-import { Card, CardContent } from "./ui/card";
 import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
 
 const AutoPlayCarousel = () => {
   return (
-    <div className="">
+    <div className="w-full h-[50vh] flex items-center justify-center rounded-lg">
       <Carousel
-        className="w-full max-w-4xl mx-auto"
+        className="w-full max-w-6xl mx-auto rounded-lg"
         opts={{
           loop: true,
           align: "start",
@@ -21,15 +21,17 @@ const AutoPlayCarousel = () => {
           }),
         ]}
       >
-        <CarouselContent>
+        <CarouselContent className="h-[50vh] rounded-lg">
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+            <CarouselItem key={index} className="relative h-[50vh] rounded-lg">
+              <div className="relative w-full h-full rounded-lg">
+                <Image
+                  src={"/img/image.png"}
+                  fill
+                  sizes="50vw"
+                  className="object-cover rounded-lg"
+                  alt="Carousel image"
+                />
               </div>
             </CarouselItem>
           ))}
