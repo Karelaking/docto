b990a5720c82f88502f418089850d6fe74732e6c
mradul kumar katiyar <dev.mradul@proton.me>
2025-09-24 23:14:06 +0530

    disabled useLightningcss

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    useLightningcss: false, // Disable Lightning CSS
+  },
 };
 
 export default nextConfig;
